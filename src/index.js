bcb78fe7b42aee231af89be6df2d4f75cc016b43
Tony Nardi <anthonygt@gmail.com>
2018-06-25 07:23:37 -0400

    Add rules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import { initGame } from "./gameLogic";
 
-function startApp() {
+function startNewGame() {
+  document.getElementById("gameMenu").classList.add("hidden");
+  document.getElementById("gameStateBoard").classList.remove("hidden");
   initGame();
 }
 
+function showRules() {
+  document.getElementById("gameMenu").classList.add("hidden");
+  document.getElementById("rules").classList.remove("hidden");
+}
+
+function startApp() {
+  document.getElementById("newGame").addEventListener("click", startNewGame);
+  document.getElementById("showRules").addEventListener("click", showRules);
+}
 window.addEventListener("DOMContentLoaded", startApp, false);
