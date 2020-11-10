dd9e53fa72632901eb73ceb22cb80d0db5217c24
Anthony Nardi <anthonygt@gmail.com>
2020-11-09 21:16:39 -0500

    update tzaar rendering

--- src/gamePieceRenderer.js
+++ src/gamePieceRenderer.js
@@ -135,6 +135,35 @@ const CENTER_COLOR = "#FDD835";
 const PLAYER_ONE_COLOR_BG = "#1E88E5";
 const PLAYER_TWO_COLOR_BG = "#212121";
 
+function drawStar(cx, cy, spikes, outerRadius, innerRadius, ctx, centerColor) {
+  var rot = Math.PI / 2 * 3;
+  var x = cx;
+  var y = cy;
+  var step = Math.PI / spikes;
+
+  ctx.beginPath();
+  ctx.moveTo(cx, cy - outerRadius)
+  for (var i = 0; i < spikes; i++) {
+    x = cx + Math.cos(rot) * outerRadius;
+    y = cy + Math.sin(rot) * outerRadius;
+    ctx.lineTo(x, y)
+    rot += step
+
+    x = cx + Math.cos(rot) * innerRadius;
+    y = cy + Math.sin(rot) * innerRadius;
+    ctx.lineTo(x, y)
+    rot += step
+  }
+  ctx.lineTo(cx, cy - outerRadius);
+  ctx.closePath();
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = '#fff';
+  ctx.stroke();
+  ctx.fillStyle = centerColor;
+  ctx.fill();
+}
+
+
 function drawGamePiece(gamePiece, canvas) {
   const context = canvas.getContext("2d");
   if (gamePiece.ownedBy === PLAYER_ONE) {
@@ -152,13 +181,8 @@ function drawGamePiece(gamePiece, canvas) {
   if (gamePiece.type === TZAAR) {
     context.fillStyle = CENTER_COLOR;
     context.beginPath();
-    context.arc(
-      circleRadius,
-      circleRadius,
-      smallerCircleRadius * .8,
-      0,
-      2 * Math.PI
-    );
+    drawStar(circleRadius, circleRadius, 4, circleRadius / 2, circleRadius / 4, context, CENTER_COLOR);
+
     context.fill();
   } else if (gamePiece.type === TZARRA) {
     context.strokeStyle = CENTER_COLOR;
